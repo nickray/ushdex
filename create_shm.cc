d09758ee545ebb7ae3fbc773ec4e9790ccae5fb1
nickray <n@skaaf.net>
2013-02-07 18:38:06 +0100

    Better test-drive

--- create_shm.cc
+++ create_shm.cc
@@ -9,6 +9,8 @@ int main ()
 
    managed_shared_memory segment(create_only, SHM_NAME, 65536);
 /*
+ * If the size is too small, we can get sudden exceptions:
+ *
    managed_shared_memory segment(create_only, "SHM_NAME", 1);
 
     terminate called after throwing an instance of 'boost::interprocess::interprocess_exception'
@@ -22,9 +24,20 @@ int main ()
    LongDataExchange * long_dex = 
        segment.construct<LongDataExchange>("LongDataExchange") (key_less(), allocator);
 
+   // Insert some double data
    double_dex->insert(DoubleValueType(KeyType("CL.GLOB.0", "last_traded_price", allocator), 9590.)); 
 
-   long_dex->insert(LongValueType(KeyType("CL.GLOB.0", "last_update", allocator), 1360258008084400896)); 
+   // Some long data too
+   KeyType my_key("CL.GLOB.0", "last_update", allocator);
+   long_dex->insert(LongValueType(my_key, 1360258008084400896)); 
+
+   // Direct access works, once the data has been located via the keys
+   long & my_last_update((*long_dex)[my_key]);
+
+   cout << "I haven't forgotten: " << my_last_update << endl;
+
+   my_last_update /= 37;
+   cout << "Manipulated: " << long_dex->at(my_key) << endl;
 
    return 0;
 }
