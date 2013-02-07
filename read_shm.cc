89690d0af7e56b299b36c7a79479f460cdd0159b
nickray <n@skaaf.net>
2013-02-07 16:19:30 +0100

    Test

---
+++ read_shm.cc
@@ -0,0 +1,21 @@
+#include "shm.h"
+
+int main ()
+{
+   shared_memory_object::remove("MySharedMemory");
+   managed_shared_memory segment
+      (open_only
+      ,"MySharedMemory" //segment name
+      ,65536);          //segment size in bytes
+
+   /*
+   ShmemAllocator alloc_inst (segment.get_segment_manager());
+
+   MyMap *mymap =
+        segment.construct<MyMap>("MyMap")      //object name
+                                   (std::less<int>() //first  ctor parameter
+                                   ,alloc_inst);     //second ctor parameter
+                                   */
+   return 0;
+}
+
