1026b5cd994c77021df8352ec828f2124cbd2628
nickray <n@skaaf.net>
2013-04-22 07:28:28 -0400

    Older Boost versions do not support cbegin/cend

--- ush_list.cc
+++ ush_list.cc
@@ -7,10 +7,10 @@ int main ()
     ush::ShmSession session(ush::connect_only);
 
     std::cout << "::DoubleDataExchange\n";
-    for (auto it = session.doubles().cbegin(); it != session.doubles().cend(); ++it)
+    for (auto it = session.doubles().begin(); it != session.doubles().end(); ++it)
         std::cout << it->first << " => " << it->second << '\n';
     std::cout << "::LongDataExchange\n";
-    for (auto it = session.longs().cbegin(); it != session.longs().cend(); ++it)
+    for (auto it = session.longs().begin(); it != session.longs().end(); ++it)
         std::cout << it->first << " => " << it->second << '\n';
 
     return 0;
